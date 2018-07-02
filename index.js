c56d4d3c60158e640e24e218d2a4629b61e310d5
Alex Chaffee <alex@stinky.com>
2018-07-02 07:31:41 -0400

    node server

---
+++ index.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+var http = require('http');
+var mime = require('mime-types'); // https://github.com/jshttp/mime-types
+
+var port = process.env.PORT || 5000;
+http.createServer(function (request, response) {
+    let contentType = 'text/plain';
+
+    var file;
+    if (request.url === '/') {
+        file = 'index.html';
+    } else if (request.url.match(/\/?style.css$/)) {
+        file = 'style.css';
+    } else {
+        file = '.' + decodeURIComponent(request.url);
+    }
+
+    let data;
+    try {
+        console.log('Serving ' + file);
+        data = fs.readFileSync(file);
+        contentType = mime.lookup(file);
+    } catch (error) {
+        console.log(error);
+        // todo: confirm this is a file not found error
+        data = "Error: " + error.toString();
+        response.statusCode = 404;
+    }
+
+    response.setHeader('Content-Type', contentType + '; charset=utf-8');
+    response.write(data);
+    response.end();
+}).listen(port);
+
+console.log("Listening on port " + port);
