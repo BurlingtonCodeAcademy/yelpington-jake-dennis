812d9aa3a3b199d045af150f47c83397c367c489
Alex Chaffee <alexch@gmail.com>
2018-07-06 10:43:51 -0700

    simplified URL path system

--- index.js
+++ index.js
@@ -5,24 +5,40 @@ var mime = require('mime-types'); // https://github.com/jshttp/mime-types
 var port = process.env.PORT || 5000;
 http.createServer(function (request, response) {
     let contentType = 'text/plain';
-
     var file;
-    path = request.url
+    let data;
+
+    let path = request.url
     path = path.split('?')[0]
 
     if (path === '/') {
         file = 'index.html';
-    } else if (path.indexOf('.') === -1) {
+    }
+    else if (path === '/all.json') {
+        let jsonFiles = fs.readdirSync('.')
+            .filter(file => file.endsWith('.json'))
+            .filter(file => !file.startsWith('package'))
+            .map(file => file.substring(0, file.length - '.json'.length))
+        file = null
+        contentType = mime.lookup('json')
+        console.log(jsonFiles)
+        data = JSON.stringify(jsonFiles)
+    } 
+    else if (path.indexOf('.') === -1) {
+        // '/foo' loads index.html too,
+        // which will then ask for '/foo.json'
         file = 'index.html';
-    } else {
+    } 
+    else {
         file = '.' + decodeURIComponent(request.url);
     }
 
-    let data;
     try {
-        console.log('Serving ' + file);
-        data = fs.readFileSync(file);
-        contentType = mime.lookup(file);
+        if (file) {
+            console.log('Serving ' + file);
+            data = fs.readFileSync(file);
+            contentType = mime.lookup(file);
+        }
     } catch (error) {
         console.log(error);
         // todo: confirm this is a file not found error
