0f0a3415d02208ae85beb839107c362fdb6df4a0
Joshua Burke <burke.joshua.james@gmail.com>
2019-03-21 17:20:57 -0400

    Remove Cypress.js from the project as there are zero tests
    In the future it would be a good idea to add tests to guide the students down
    the path of least resistance.

--- cypress/integration/0_sanity.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Cypress', function () {
-  it('successfully loads and asserts', function () {
-    expect(true).to.equal(true);
-  });
-  it('successfully visits the home page', function () {
-    cy.visit('/');
-  });
-});
-
